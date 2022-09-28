56acbd4e275a70d69893a586a714196093957e7d
DK <spol@naver.com>
2022-09-28 21:03:31 +0900

    회원가입 xml 꾸미기

--- app/src/main/java/com/example/iwannameetsomeone/auth/IntroActivity.kt
+++ app/src/main/java/com/example/iwannameetsomeone/auth/IntroActivity.kt
@@ -1,12 +1,23 @@
 package com.example.iwannameetsomeone.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.iwannameetsomeone.R
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        signupBtn.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+
+        }
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/iwannameetsomeone/auth/SignupActivity.kt
@@ -0,0 +1,16 @@
+package com.example.iwannameetsomeone.auth
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.iwannameetsomeone.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+
+    }
+}
