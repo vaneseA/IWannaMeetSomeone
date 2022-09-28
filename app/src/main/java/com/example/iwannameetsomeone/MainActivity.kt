7584ed91a5bb76100c5566f22fa082c39a61258c
DK <spol@naver.com>
2022-09-28 19:15:22 +0900

    CardStackView 구현

--- app/src/main/java/com/example/iwannameetsomeone/MainActivity.kt
+++ app/src/main/java/com/example/iwannameetsomeone/MainActivity.kt
@@ -2,10 +2,58 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.iwannameetsomeone.slider.CardStackAdapter
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
 
-class MainActivity : AppCompatActivity() {
+  class MainActivity : AppCompatActivity() {
+
+    lateinit var cardStackAdapter: CardStackAdapter
+    lateinit var manager : CardStackLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardStackView = findViewById<CardStackView>(R.id.cardStackView)
+
+        manager = CardStackLayoutManager(baseContext,object : CardStackListener{
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+
+            }
+
+            override fun onCardRewound() {
+
+            }
+
+            override fun onCardCanceled() {
+
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+
+            }
+
+        })
+        val  tesList = mutableListOf<String>()
+        tesList.add("a")
+        tesList.add("b")
+        tesList.add("c")
+
+
+        cardStackAdapter = CardStackAdapter(baseContext, tesList)
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = cardStackAdapter
     }
 }
