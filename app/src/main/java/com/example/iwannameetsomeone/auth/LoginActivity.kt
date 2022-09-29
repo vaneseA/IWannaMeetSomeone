0f20cebc7b9e943e39b37b6a67b77d406bf79be0
DK <spol@naver.com>
2022-09-29 19:34:16 +0900

    activity_login.xml수정

--- app/src/main/java/com/example/iwannameetsomeone/auth/LoginActivity.kt
+++ app/src/main/java/com/example/iwannameetsomeone/auth/LoginActivity.kt
@@ -3,21 +3,43 @@ package com.example.iwannameetsomeone.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.example.iwannameetsomeone.MainActivity
 import com.example.iwannameetsomeone.R
-import kotlinx.android.synthetic.main.activity_intro.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+
+private lateinit var auth: FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_login)
 
-        signupBtn.setOnClickListener {
-            startActivity(Intent(this, SignupActivity::class.java))
+        auth = Firebase.auth
 
-        }
         loginBtn.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
+
+            val email = findViewById<EditText>(R.id.emailEdt)
+            val pwd = findViewById<EditText>(R.id.passwordEdt)
+
+            auth.signInWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+
+            signupBtn.setOnClickListener {
+                startActivity(Intent(this, SignupActivity::class.java))
+
+            }
         }
     }
 }
