dda438b51fdf01db23fdbedeb8b5bca316fa706d
DK <spol@naver.com>
2022-09-30 03:39:46 +0900

    데이터베이스에서 받아온 정보를 어댑터를 통해 메인액티비티에 뿌려줌

--- app/src/main/java/com/example/iwannameetsomeone/auth/SignupActivity.kt
+++ app/src/main/java/com/example/iwannameetsomeone/auth/SignupActivity.kt
@@ -3,20 +3,26 @@ package com.example.iwannameetsomeone.auth
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.addTextChangedListener
 import com.example.iwannameetsomeone.MainActivity
 import com.example.iwannameetsomeone.R
 import com.example.iwannameetsomeone.utils.FirebaseRef
-import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_signup.*
+import java.io.ByteArrayOutputStream
 
 private val TAG = "SignupActivity"
 
@@ -27,14 +33,33 @@ private var city = ""
 private var age = ""
 private var nickname = ""
 
+
 class SignupActivity : AppCompatActivity() {
+
+    var isPwDupOk = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         // Initialize Firebase Auth
         auth = Firebase.auth
 
-        val signupSaveBtn = findViewById<TextView>(R.id.signupSaveBtn)
+
+        val getAction = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback { uri ->
+                profileImg.setImageURI(uri)
+            }
+        )
+        profileImgBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            getAction.launch("image/*")
+            //기존 버튼 배경 사라지게
+            profileImgBtn.alpha =0f
+
+            startActivityForResult(intent, 0)
+        }
+
         signupSaveBtn.setOnClickListener {
 
             val email = findViewById<EditText>(R.id.emailEdt)
@@ -44,23 +69,80 @@ class SignupActivity : AppCompatActivity() {
             age = findViewById<EditText>(R.id.ageEdt).text.toString()
             nickname = findViewById<EditText>(R.id.nickEdt).text.toString()
 
+            if (email.text?.isEmpty()!!) {
+                Toast.makeText(this, "email을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (!isPwDupOk) {
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (nickname.isEmpty()!!) {
+                Toast.makeText(this, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else if (age.isEmpty()!!) {
+                Toast.makeText(this, "나이를 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else if (city.isEmpty()!!) {
+                Toast.makeText(this, "지역을 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else {
 
-            auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        val user = auth.currentUser
-                        uid = user?.uid.toString()
-                        val userModel = UserDataModel(uid, nickname, age, gender, city)
+                auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
 
-                        //데이터베이스에 저장하기
-                        FirebaseRef.userInfoRef.child(uid).setValue(userModel)
-//                        startActivity(Intent(this, MainActivity::class.java))
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                            val user = auth.currentUser
+                            uid = user?.uid.toString()
 
+                            val userModel = UserDataModel(
+                                uid,
+                                nickname,
+                                age,
+                                gender,
+                                city
+                            )
+
+                            //데이터베이스에 저장하기
+                            FirebaseRef.userInfoRef.child(uid).setValue(userModel)
+                            uploadImage(uid)
+                            startActivity(Intent(this, MainActivity::class.java))
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
+
+                        }
                     }
-                }
+
+            }
+        }
+
+        passwordEdt.addTextChangedListener {
+            isPwDupOk = (it.toString() == pwDupCheckEdt.text.toString())
+        }
+        pwDupCheckEdt.addTextChangedListener {
+            isPwDupOk = (it.toString() == passwordEdt.text.toString())
         }
     }
+
+    private fun uploadImage(uid: String) {
+
+        val storage = Firebase.storage
+        val storageRef = storage.reference.child(uid + ".png")
+
+
+        // Get the data from an ImageView as bytes
+        profileImg.isDrawingCacheEnabled = true
+        profileImg.buildDrawingCache()
+        val bitmap = (profileImg.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = storageRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
+
+
+    }
+
 }
--- app/src/main/java/com/example/iwannameetsomeone/slider/CardStackAdapter.kt
+++ app/src/main/java/com/example/iwannameetsomeone/slider/CardStackAdapter.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.iwannameetsomeone.R
 import com.example.iwannameetsomeone.auth.UserDataModel
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class CardStackAdapter(val context: Context, val items: List<UserDataModel>) :
     RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
@@ -27,6 +32,7 @@ class CardStackAdapter(val context: Context, val items: List<UserDataModel>) :
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        val image = itemView.findViewById<ImageView>(R.id.profileImageArea)
         val nickname = itemView.findViewById<TextView>(R.id.itemNickname)
         val age = itemView.findViewById<TextView>(R.id.itemAge)
         val city = itemView.findViewById<TextView>(R.id.itemCity)
@@ -34,6 +40,17 @@ class CardStackAdapter(val context: Context, val items: List<UserDataModel>) :
 
         fun binding(data: UserDataModel) {
 
+            val storageRef = Firebase.storage.reference.child(data.uid + ".png")
+            storageRef.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+
+                if(task.isSuccessful) {
+                    Glide.with(context)
+                        .load(task.result)
+                        .into(image)
+
+                }
+
+            })
             nickname.text = data.nickname
             age.text = data.age
             city.text = data.city
