90df0b0cbd49a65291544dc560dae54df13c05f2
DK <spol@naver.com>
2022-10-04 19:09:50 +0900

    성별에 따른 결과 로직

--- app/src/main/java/com/example/iwannameetsomeone/MainActivity.kt
+++ app/src/main/java/com/example/iwannameetsomeone/MainActivity.kt
@@ -6,21 +6,25 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import com.bumptech.glide.Glide
 import com.example.iwannameetsomeone.auth.LoginActivity
 import com.example.iwannameetsomeone.auth.UserDataModel
 import com.example.iwannameetsomeone.settings.MyPageActivity
 import com.example.iwannameetsomeone.slider.CardStackAdapter
+import com.example.iwannameetsomeone.utils.FirebaseAuthUtils
 import com.example.iwannameetsomeone.utils.FirebaseRef
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
 import com.yuyakaido.android.cardstackview.Direction
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_my_page.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +34,12 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
     private val usersDataList = mutableListOf<UserDataModel>()
+
+    private var userCount = 0
+
+    private var uid = FirebaseAuthUtils.getUid()
+
+    private lateinit var curruntUserGender: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,6 +68,19 @@ class MainActivity : AppCompatActivity() {
 
             override fun onCardSwiped(direction: Direction?) {
 
+                if(direction == Direction.Right) {
+
+                }
+                if(direction == Direction.Left) {
+
+                }
+
+                userCount = userCount + 1
+
+
+                if(userCount == usersDataList.count()) {
+                    getUserDataList(curruntUserGender)
+                }
             }
 
             override fun onCardRewound() {
@@ -81,10 +104,35 @@ class MainActivity : AppCompatActivity() {
         cardStackAdapter = CardStackAdapter(baseContext, usersDataList)
         cardStackView.layoutManager = manager
         cardStackView.adapter = cardStackAdapter
-        getUserDataList()
+
+//        getUserDataList()
+        getMyUserData()
     }
 
-    private fun getUserDataList() {
+
+    private fun getMyUserData(){
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                Log.d(TAG, dataSnapshot.toString())
+                val data = dataSnapshot.getValue(UserDataModel::class.java)
+
+                Log.d(TAG, data?.gender.toString())
+
+                curruntUserGender = data?.gender.toString()
+
+                getUserDataList(curruntUserGender)
+
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FirebaseRef.userInfoRef.child(uid).addValueEventListener(postListener)
+    }
+
+    private fun getUserDataList(curruntUserGender : String) {
 
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -92,7 +140,15 @@ class MainActivity : AppCompatActivity() {
                 for (dataModel in dataSnapshot.children) {
 
                     val user = dataModel.getValue(UserDataModel::class.java)
-                    usersDataList.add(user!!)
+
+                    if(user!!.gender.toString().equals(curruntUserGender)){
+
+                    }else{
+
+                        usersDataList.add(user!!)
+                    }
+
+
 
                 }
 
